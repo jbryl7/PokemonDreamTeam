ce67e538ffcd71ad3dc743f05db0878f48c26ace
jbryl7 <jakub.bryl7@gmail.com>
2020-04-21 20:11:21 +0200

    fight results matrice

--- Pokemon.py
+++ Pokemon.py
@@ -26,18 +26,26 @@ class Pokemon:
             "steel": float(row[17]),
             "water": float(row[18]),
         }
-        self.attack = row[19]
-        self.happines = row[21]
-        self.base_total = row[22]
-        self.capture_rate = row[23]
-        self.defense = row[25]
-        self.health = row[28]
-        self.s_attack = row[32]
-        self.s_defense = row[33]
-        self.is_legendary = row[39]
+        self.attack = float(row[19])
+        self.happines = float(row[21])
+        self.base_total = float(row[22])
+        self.capture_rate = float(row[23])
+        self.defense = float(row[25])
+        self.health = float(row[28])
+        self.s_attack = int(row[32])
+        self.s_defense = int(row[33])
+        self.is_legendary = int(row[39])
+
     def hits_needed_to_be_defeated(self, other_pokemon):
-        k_attacks = 3
-        return self.health / (other_pokemon.attack * self.get_defense_multiplier(other_pokemon) / (self.defense / 2.0)) + other_pokemon.happines * k_attacks
+        k_attacks = 5
+        hits_needed = self.health / self.hit_dmg(other_pokemon) + other_pokemon.happines * k_attacks
+        if self.is_legendary:
+            hits_needed *= 0.3
+        # print((hits_needed, self.hit_dmg(other_pokemon), self.get_defense_multiplier(other_pokemon)))
+        return hits_needed
+
+    def hit_dmg(self, other_pokemon):
+        return other_pokemon.attack / (self.defense * self.get_defense_multiplier(other_pokemon))
 
     def fight_result(self, other_pokemon):
         p1 = self.hits_needed_to_be_defeated(other_pokemon)
@@ -54,9 +62,9 @@ class Pokemon:
         attacker_types = sorted(attacker.types, key=lambda k: self.against[k])
         if len(attacker_types) == 2:
             p1, p2 = 0.35, 0.65
-            return self.against[attacker_types[0]] * p1 + self.against[attacker_types[1]] * p2    
+            return max(0.1, self.against[attacker_types[0]] * p1 + self.against[attacker_types[1]] * p2)
         else:
-            return self.against[attacker_types[0]]
+            return max(0.1, self.against[attacker_types[0]])
 
     def __str__(self):
         return str(self.__dict__)
--- PokemonList.py
+++ PokemonList.py
@@ -3,20 +3,38 @@ import pandas as pd
 from Pokemon import Pokemon
 
 class PokemonList:
-    def __init__(self, path_to_data):
-        df_pokemon = pd.read_csv(path_to_data)
-        df_pokemon.set_index('pokedex_number', inplace=True)
-        df_pokemon['type2'].fillna(value='nan', method = None, inplace=True)
-        self.pokemons = list()
-        for i in range(1, 802):
-            self.pokemons.append(Pokemon(df_pokemon.loc[i], i))
-        self.fight_results = np.empty((len(self.pokemons), len(self.pokemons)))
+    def __init__(self, pokemon_list, path_to_data):
+        if (path_to_data is None):
+            self.pokemons = pokemon_list
+        else:
+            df_pokemon = pd.read_csv(path_to_data)
+            df_pokemon.set_index('pokedex_number', inplace=True)
+            df_pokemon['type2'].fillna(value='nan', method = None, inplace=True)
+            self.clean_capture_rate(df_pokemon)
+            self.normalize(df_pokemon)
+            self.pokemons = list()
+            for i in range(1, 802):
+                self.pokemons.append(Pokemon(df_pokemon.loc[i], i))
+        self.fight_results = np.empty((len(self.pokemons) + 1, len(self.pokemons) + 1))
         self.initialize_fight_results()
-
+        self.sum_of_fight_results = np.sum(self.fight_results, where=self.fight_results > 0.5, axis=1)
+        
 
     def initialize_fight_results(self):
         for i in range(len(self.pokemons)):
             for j in range(i, len(self.pokemons)):
-                self.fight_results[i] = self.pokemons[i].fight_result(self.pokemons[j])
+                self.fight_results[i][j] = self.pokemons[i].fight_result(self.pokemons[j])
+        self.fight_results.fill
     
-    
+    def clean_capture_rate(self, df):
+        df.loc[df.capture_rate == '30 (Meteorite)255 (Core)', 'capture_rate'] = 35
+        df['capture_rate'] = df.capture_rate.astype('int')
+
+    def normalize(self, df):
+        columns_to_update = ['capture_rate', 'base_happiness']
+        print(columns_to_update)
+        for col in columns_to_update:
+            if col == 'base_happiness':
+                df[col].update((df[col] - df[col].min()) / (df[col].max() - df[col].min()) - 0.5)
+            else:
+                df[col].update((df[col] - df[col].min()) / (df[col].max() - df[col].min()))
